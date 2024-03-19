5b1ec408a16b7a785bb6621077b965517cc9cd3a
Hardik Gupta <thothgreen@gmail.com>
2024-03-20 02:04:18 +0530

    With error handling

--- src/index.js
+++ src/index.js
@@ -6,6 +6,29 @@ dotenv.config({
     path: './env'
 })
 dbConnect()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error: ", error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Server is running at port: ", process.env.PORT)
+    })
+})
+.catch((err) => {
+    console.log("Error connecting to DB !!! ", err)
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
